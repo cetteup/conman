083e31e0478280d5309312765c9673a927506d7b
cetteup <me@cetteup.com>
2022-09-27 00:20:24 +0200

    tag: Bump version to 0.1.4

--- cmd/bf2-conman/internal/gui/main_window.go
+++ cmd/bf2-conman/internal/gui/main_window.go
@@ -191,7 +191,7 @@ func CreateMainWindow(h *handler.Handler, profiles []game.Profile, defaultProfil
 				},
 			},
 			declarative.Label{
-				Text:       "BF2 conman v0.1.3",
+				Text:       "BF2 conman v0.1.4",
 				Alignment:  declarative.AlignHCenterVCenter,
 				TextColor:  walk.Color(win.GetSysColor(win.COLOR_GRAYTEXT)),
 				Background: declarative.SolidColorBrush{Color: walk.Color(win.GetSysColor(win.COLOR_BTNFACE))},
